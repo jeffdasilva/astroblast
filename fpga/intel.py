1ee014f3f6eb79aa968ca8d7fca263ffc815de43
Jeff DaSilva <jeff_dasilva@hotmail.com>
2016-11-26 13:33:38 -0800

    swith to use multiple inheritance for IntelFpga generators

--- fpga/intel.py
+++ fpga/intel.py
@@ -5,22 +5,19 @@ from gen.file import FileGenerator
 from gen.generate import Generator
 from gen.project import ProjectGenerator
 
-# decorate the generator classes by overriding the templates directory
-
+# Decorate the generator classes by overriding the templates directory
 class IntelFpgaGenerator(Generator):
     def __init__(self):
         super(IntelFpgaGenerator, self).__init__()
         self.template_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep + "templates" + os.sep + "intelFPGA"
 
-class IntelFpgaFileGenerator(FileGenerator):
+class IntelFpgaFileGenerator(FileGenerator, IntelFpgaGenerator):
     def __init__(self, output_file=None):
         super(IntelFpgaFileGenerator, self).__init__(output_file=output_file)
-        self.template_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep + "templates" + os.sep + "intelFPGA"
 
-class IntelFpgaProjectGenerator(ProjectGenerator):
+class IntelFpgaProjectGenerator(ProjectGenerator, IntelFpgaFileGenerator):
     def __init__(self, output_dir=None):
         super(IntelFpgaProjectGenerator, self).__init__(output_dir=output_dir)
-        self.template_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep + "templates" + os.sep + "intelFPGA"
 
 ###############################################################################
 #
@@ -30,14 +27,21 @@ class IntelFpgaProjectGenerator(ProjectGenerator):
 class TestIntelFpgaGenerator(unittest.TestCase):
 
     def testConstructor(self):
+
+        expected_template_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep + "templates" + os.sep + "intelFPGA"
+
         g = IntelFpgaGenerator()
         self.assertNotEqual(g, None)
+        self.assertEqual(g.template_dir, expected_template_dir)
 
         fg = IntelFpgaFileGenerator()
         self.assertNotEqual(fg, None)
+        self.assertEqual(fg.template_dir, expected_template_dir)
 
         pg = IntelFpgaProjectGenerator()
         self.assertNotEqual(pg, None)
+        self.assertEqual(pg.template_dir, expected_template_dir)
+
 
     def testTemplateDir(self):
 
